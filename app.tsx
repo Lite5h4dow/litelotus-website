226f978eeb92a51bb80cba5ec7a926fec9c92800
Lamar Daughma <lite5h4dow@gmail.com>
2021-08-01 16:36:23 +0100

    forgot the meta tags

--- app.tsx
+++ app.tsx
@@ -5,6 +5,8 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
     <main>
       <head>
         <meta name="viewport" content="width=device-width" />
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+        <meta name="description" content="Creating apps and mods for games and every day use"/>
       </head>
       <Page {...pageProps} />
     </main>
