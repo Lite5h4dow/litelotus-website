b228cb97ed57f19b7f572e1e3f14fc4b7e0959ba
Lamar Daughma <lite5h4dow@gmail.com>
2021-08-08 21:57:15 +0100

    button indentation was fucked

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -15,10 +15,10 @@ const Button = (props: IButton) => {
   const {theme} = useTheme()
 
   return (
-<button {...props} className={`button ${theme} ${props.className ? props.className : ""} ${props.transparent?"transparent":""} ${props.centered?"centered":""}`.trim()}>
-    {!!props.icon?props.icon:null}
-    {typeof props.children === "string"? <p>{props.children}</p>: props.children}
-  </button>
+    <button {...props} className={`button ${theme} ${props.className ? props.className : ""} ${props.transparent?"transparent":""} ${props.centered?"centered":""}`.trim()}>
+      {!!props.icon?props.icon:null}
+      {typeof props.children === "string"? <p>{props.children}</p>: props.children}
+    </button>
   );
 };
 
