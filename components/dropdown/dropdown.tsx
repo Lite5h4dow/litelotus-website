2a6563ad4ac451736dec56f414f39b4f3e7b6c81
Lamar Daughma <lite5h4dow@gmail.com>
2021-08-09 23:01:49 +0100

    dropdown menu added and cleaned up layout

---
+++ components/dropdown/dropdown.tsx
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import Button from "../button/button.tsx";
+import useTheme from "~/lib/hooks/useTheme.tsx";
+import "./dropdown.css"
+
+const Dropdown = (props:any) => {
+  const {items} = props;
+
+  const [show, setShow] = useState(false);
+  const {theme} = useTheme();
+
+  return(
+    <div className="dropdown-container">
+      <Button {...props} 
+        onFocus={()=>{
+          props.onFocus && props.onFocus();
+          setShow(true);
+        }} 
+        onBlur={()=>{
+          props.onBlur && props.onBlur();
+          setShow(false);
+        }}/>
+      <div className={`dropdown ${show?"show":""} ${theme}`}>
+        {items.map((item:JSX.Element, index:number)=> <div key={index}>{item}</div>)}
+      </div>      
+    </div>
+  );
+};
+
+export default Dropdown
